032d5b260bdd7b1ef3448a78e7a6eac83407373e
Takako <24834291+HelloTakako@users.noreply.github.com>
2024-05-23 01:09:53 -0700

    displayed char in the drawing pad

--- components/DrawingPad.jsx
+++ components/DrawingPad.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import RandomMarathiAlphabetImages from './RandomMarathiAlphabetImages';
+import { CharacterData } from '../lib/context/MarathiCharContext';
 
 const DrawingPad = () => {
+  const { character } = useContext(CharacterData);
   const canvasRef = useRef(null);
 
   const handleClear = () => {
@@ -19,20 +21,27 @@ const DrawingPad = () => {
 
   return (
     <div>
-      <RandomMarathiAlphabetImages handleClear={handleClear}  />
-      <div className='flex justify-center w-full overflow-hidden border-[2px]'>
-        <CanvasDraw 
-          ref={canvasRef}
-          brushRadius={2}
-          lazyRadius={0}
-          brushColor="#000" 
-        />
+      <RandomMarathiAlphabetImages handleClear={handleClear} />
+      <div className="relative flex justify-center w-full overflow-hidden border-[2px]">
+        {character && <p className='top-1/2 -translate-y-1/2 text-[350px] absolute opacity-10 -z-1'>{character.char}</p>}
+        <div className='relative canvas-wrapper'>
+          <CanvasDraw
+            ref={canvasRef}
+            brushRadius={2}
+            lazyRadius={0}
+            brushColor="#000"
+          />
+        </div>
       </div>
 
-      <br/>
-      <div className='w-full flex justify-center'>
-        <button className="button" onClick={handleClear}>Clear</button>
-        <button className="button" onClick={handleUndo}>Undo</button>
+      <br />
+      <div className="w-full flex justify-center">
+        <button className="button" onClick={handleClear}>
+          Clear
+        </button>
+        <button className="button" onClick={handleUndo}>
+          Undo
+        </button>
       </div>
     </div>
   );
